8f96c571574d19168363d37ac85710900210f638
MikaChe <mikhail.cherkas@gmail.com>
2020-12-13 04:08:19 +0100

    Testing Git repository - 2 files

---
+++ query201.sql
@@ -0,0 +1,10 @@
+#standardSQL
+SELECT
+geoNetwork_city,
+totals_transactions,
+fullVisitorId,
+SUM(totals_transactions) AS total_products_ordered,
+COUNT( DISTINCT fullVisitorId) AS distinct_visitors,
+SUM(totals_transactions) / COUNT( DISTINCT fullVisitorId) AS avg_products_ordered
+FROM
+`data-to-insights.ecommerce.rev_transactions`
