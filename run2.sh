55788ccfccf50d90893a3b913b9ab10e149b93eb
“MikaCloud” <mikhail.cherkas@gmail.com>
2021-07-11 04:27:01 +0200

    Add main files and folders for GSP418

---
+++ run2.sh
@@ -0,0 +1,14 @@
+PROJECT_ID=$(gcloud info --format='value(config.project)')
+gsutil mb gs://$PROJECT_ID
+
+export INSTANCE_NAME="tensorflow2020"
+export VM_IMAGE_PROJECT="deeplearning-platform-release"
+#export VM_IMAGE_FAMILY="tf2-2-2-cpu"
+export VM_IMAGE_FAMILY="tf-latest-cpu"
+export MACHINE_TYPE="n1-standard-4"
+export LOCATION="us-central1-b"
+
+gcloud beta notebooks instances create $INSTANCE_NAME \
+--vm-image-project=$VM_IMAGE_PROJECT \
+--vm-image-family=$VM_IMAGE_FAMILY \
+--machine-type=$MACHINE_TYPE --location=$LOCATION 
