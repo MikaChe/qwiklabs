d60456a40672fef660bc9884a3a30ae1f8e9e05e
“MikaCloud” <mikhail.cherkas@gmail.com>
2021-07-12 04:18:39 +0200

    Update for GSP418

--- run2.sh
+++ run2.sh
@@ -1,10 +1,13 @@
 PROJECT_ID=$(gcloud info --format='value(config.project)')
 gsutil mb gs://$PROJECT_ID
 
+gcloud services enable notebooks.googleapis.com compute.googleapis.com oslogin.googleapis.com
+
 export INSTANCE_NAME="tensorflow2020"
 export VM_IMAGE_PROJECT="deeplearning-platform-release"
 #export VM_IMAGE_FAMILY="tf2-2-2-cpu"
-export VM_IMAGE_FAMILY="tf-latest-cpu"
+#export VM_IMAGE_FAMILY="tf-latest-cpu"
+export VM_IMAGE_FAMILY="tf-1-15-cpu"
 export MACHINE_TYPE="n1-standard-4"
 export LOCATION="us-central1-b"
 
